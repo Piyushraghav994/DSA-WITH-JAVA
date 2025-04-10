5aad85f4f78e166f6aeeb0e2efe0d360d92ef24f
Piyush raghav <141063566+Piyushraghav994@users.noreply.github.com>
2025-04-10 17:26:38 +0530

    done

--- Strings/String2.java
+++ Strings/String2.java
@@ -1,4 +1,5 @@
 
+import java.util.Scanner;
 import java.util.*;
 
 public class String2 {
@@ -12,19 +13,45 @@ public class String2 {
      * }
      */
 
-    public static boolean Palindrome(String str) {
+    /*
+     * public static boolean Palindrome(String str) {
+     * 
+     * int n = str.length();
+     * boolean flag = false;
+     * 
+     * for (int i = 0; i < str.length() / 2; i++) { // Loop will run at half of the
+     * String length
+     * 
+     * if (str.charAt(i) == str.charAt(n - 1 - i)) {
+     * return flag = true;
+     * }
+     * 
+     * }
+     * return flag;
+     * }
+     */
 
+    public static float getshortest(String str) {
+        int x = 0, y = 0;
         int n = str.length();
-        boolean flag = false;
+        for (int i = 0; i < n; i++) {
+            char direct = str.charAt(i);
 
-        for (int i = 0; i < str.length() / 2; i++) { // Loop will run at half of the String length
-
-            if (str.charAt(i) == str.charAt(n - 1 - i)) {
-                return flag = true;
+            if (direct == 'N' || direct == 'n') {
+                x++;
+            } else if (direct == 'S' || direct == 's') {
+                y--;
+            } else if (direct == 'E' || direct == 'e') {
+                x++;
+            } else {
+                x--;
             }
-
         }
-        return flag;
+        int X2 = x * x;
+        int Y2 = y * y;
+
+        return (float) Math.sqrt(X2 + Y2);
+
     }
 
     public static void main(String args[]) {
@@ -34,6 +61,8 @@ public class String2 {
         String fullname = sc.nextLine();
 
         // PrintLetter(fullname);
-        System.out.println("Is it palindrome ? :- " + Palindrome(fullname));
+        // System.out.println("Is it palindrome ? :- " + Palindrome(fullname));
+        // System.out.println(getshortest(fullname));
+
     }
 }
